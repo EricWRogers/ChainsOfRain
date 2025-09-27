32e39c3eadfde0803a0f6df77fd7ff43355244c2
Logan <Loganhobson31@gmail.com>
2025-09-26 20:17:56 -0500

    Started on guns

---
+++ Assets/Scripts/Gunbase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Gunbase : MonoBehaviour
+{
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Pickup();
+    }
+    private void Pickup()
+    {
+
+    }
+}
