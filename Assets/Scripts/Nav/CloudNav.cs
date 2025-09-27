47a5642fe297dce5fe37e255c1ad246c9d1d9e99
EricWRogers <ericwilliamrogers@gmail.com>
2025-09-26 23:07:25 -0500

    Generate Point Cloud

---
+++ Assets/Scripts/Nav/CloudNav.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(CloudNav))]
+public class CloudNavEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        CloudNav cloudNav = (CloudNav)target;
+
+        if (GUILayout.Button("Spawn Cloud"))
+        {
+            cloudNav.SpawnCloud();
+        }
+    }
+}
+#endif
+
+public class CloudNav : MonoBehaviour
+{
+    public GameObject cloudPointPrefab;
+
+    public int xCount = 50;
+    public int yCount = 10;
+    public int zCount = 50;
+
+    public void SpawnCloud()
+    {
+        while (transform.childCount > 0)
+        {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+
+        int halfXCount = xCount / 2;
+        int halfYCount = yCount / 2;
+        int halfZCount = zCount / 2;
+        for (int x = -halfXCount; x < halfXCount; x++)
+        {
+            for (int y = -halfYCount; y < halfYCount; y++)
+            {
+                for (int z = -halfZCount; z < halfZCount; z++)
+                {
+                    GameObject pointObject = Instantiate(
+                        cloudPointPrefab,
+                        new Vector3(x, y, z),
+                        Quaternion.identity,
+                        transform);
+                }
+            }
+        }
+    }
+}
