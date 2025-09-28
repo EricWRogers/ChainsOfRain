2e6c3ac7757855cb2393387e54a92b30e6574cff
EricWRogers <ericwilliamrogers@gmail.com>
2025-09-27 23:10:01 -0500

    BigBoss

--- Assets/Scripts/Enemy/EnemyDirector.cs
+++ Assets/Scripts/Enemy/EnemyDirector.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using SuperPupSystems.Helper;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyDirector : MonoBehaviour
 {
@@ -11,18 +11,19 @@ public class EnemyDirector : MonoBehaviour
     public List<Transform> airSpawnPoints;
 
     [Header("Prefabs")]
-    public GameObject bossPrefabs;
+    public GameObject bossPrefab;
     public List<GameObject> enemyPrefabs;
     public List<GameObject> flyingEnemyPrefabs;
 
     public int targetSpending = 10;
     public int currentSpending = 0;
     public CloudNav cloudNav;
+    public UnityEvent bossOutOfHealth;
     public void Spawn()
     {
         if (currentSpending >= targetSpending)
             return;
-            
+
         // select traversal type
         EnemyTraversalType traversalType = (EnemyTraversalType)Random.Range(0, 2);
         GameObject prefab;
@@ -47,23 +48,35 @@ public class EnemyDirector : MonoBehaviour
 
         if (traversalType == EnemyTraversalType.FLYING)
         {
-            Debug.Log("cloud nav");
             enemy.GetComponent<FlyGun>().cloudNav = cloudNav;
         }
 
 
         // spend/connect event
-            currentSpending += info.cost;
-        health.outOfHealth.AddListener(() => OnEnemyDeath(info.cost));
+        currentSpending += info.cost;
+        health.outOfHealth.AddListener(() => OnEnemyDeath(info.cost, enemy.transform.position, info.drop));
     }
 
     public void SpawnBoss()
     {
+        // spawn
+        GameObject enemy = Instantiate(bossPrefab, bossSpawn.position, Quaternion.identity, transform);
+        EnemyInfo info = enemy.GetComponent<EnemyInfo>();
+        Health health = enemy.GetComponent<Health>();
 
+        if (info.traversalType == EnemyTraversalType.FLYING)
+        {
+            enemy.GetComponent<FlyGun>().cloudNav = cloudNav;
+        }
+
+        // spend/connect event
+        currentSpending += info.cost;
+        health.outOfHealth.AddListener(() => bossOutOfHealth.Invoke());
     }
 
-    public void OnEnemyDeath(int _refund)
+    public void OnEnemyDeath(int _refund, Vector3 _position, GameObject _drop)
     {
         currentSpending -= _refund;
+        Instantiate(_drop, _position, Quaternion.identity);
     }
 }
--- Assets/Scripts/Enemy/EnemyInfo.cs
+++ Assets/Scripts/Enemy/EnemyInfo.cs
@@ -4,6 +4,7 @@ public class EnemyInfo : MonoBehaviour
 {
     public EnemyTraversalType traversalType = EnemyTraversalType.GROUND;
     public int cost = 1;
+    public GameObject drop;
 }
 
 public enum EnemyTraversalType
