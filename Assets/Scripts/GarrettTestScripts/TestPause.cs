0e1e6174424f18b1f0def5a3e324382e4d7f2c7c
Garrett Shipp <garwad9@gmail.com>
2025-09-28 08:44:18 -0500

    pause fix

--- Assets/Scripts/GarrettTestScripts/TestPause.cs
+++ Assets/Scripts/GarrettTestScripts/TestPause.cs
@@ -5,7 +5,7 @@ public class TestPause : MonoBehaviour
     public PauseMenu pauseMenu;
     void Start()
     {
-        pauseMenu = GameObject.Find("PauseMenuGo").GetComponentInChildren<PauseMenu>();
+        pauseMenu = GameObject.Find("PauseMenuPrefab").GetComponentInChildren<PauseMenu>();
     }
 
     void Update()
