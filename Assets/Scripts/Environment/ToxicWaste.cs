108ad44788f74364234466e7a82fd4fe278afa83
Aiden Thacker <aidenthacker86@gmail.com>
2025-10-24 01:01:55 -0500

    Did some work on level 3
    not ready

--- Assets/Scripts/Environment/ToxicWaste.cs
+++ Assets/Scripts/Environment/ToxicWaste.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using SuperPupSystems.Helper;
 
 public class ToxicWaste : MonoBehaviour
 {
+    public int damage = 2;
+    float tickTime = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,20 @@ public class ToxicWaste : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            tickTime -= Time.deltaTime;
+
+            if (tickTime <= 0f)
+            {
+                other.GetComponent<Health>().Damage(damage);
+                tickTime = 2.0f;
+            }
+        }
     }
 }
