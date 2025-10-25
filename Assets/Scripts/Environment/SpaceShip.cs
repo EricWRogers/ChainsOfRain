5bb13d81256e8e7459be6b20a1233474da139b45
Aiden Thacker <aidenthacker86@gmail.com>
2025-10-25 10:54:41 -0500

    Space Ship for the Win

--- Assets/Scripts/Environment/SpaceShip.cs
+++ Assets/Scripts/Environment/SpaceShip.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Animator anim;
 
-    // Update is called once per frame
-    void Update()
+    public void AfterBossDeath()
     {
-        
+        anim.Play("DropContainers");
     }
 }
